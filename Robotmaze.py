e9181ea0e554b69531eaa8d7d29e5b76c4c0dcf5
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-04-04 12:55:06 +1100

    feat: adding the robot and goal's position to the representation

--- Robotmaze.py
+++ Robotmaze.py
@@ -13,18 +13,24 @@ with open(filename) as f:
     for line in f:
         content = line.strip()
         if content.startswith("[") and content.endswith("]"):
-            # Extract maze dimensions [11, 5]
+            # Extract maze dimensions 
             dimensions_str = content[1:-1]
             lengthOfMaze, widthOfMaze = map(int, dimensions_str.split(","))
-            maze = [[0 for _ in range(widthOfMaze)] for _ in range(lengthOfMaze)]
+            # "_" is a throwaway variable
+            maze = [['0' for _ in range(widthOfMaze)] for _ in range(lengthOfMaze)]
 
         elif content.startswith("(") and content.endswith(")"):
             # Extract robot position
             if robotPosition is None:
                 robotPosition = tuple(map(int, content[1:-1].split(",")))
+                x, y = robotPosition
+                maze[y][x] = "R"  # Mark robot's initial position as "R"
             else:
                 # Extract goal position
                 goalPositions.extend(tuple(map(int, x.strip()[1:-1].split(","))) for x in content.split("|"))
+                for goal in goalPositions:
+                    x, y = goal
+                    maze[y][x] = "g"  # Mark goal positions as "g"
 
                 # After reading a line for goal positions, now read for obstacle positions
                 for next_line in f:
@@ -37,7 +43,7 @@ with open(filename) as f:
                         for i in range(y, y + height):
                             for j in range(x, x + width):
                                 if 0 <= i < lengthOfMaze and 0 <= j < widthOfMaze:
-                                    maze[i][j] = 1
+                                    maze[i][j] = "1"  # Mark obstacle positions as "1"
                     else:
                         # Stop reading obstacle positions if a new section starts
                         break
